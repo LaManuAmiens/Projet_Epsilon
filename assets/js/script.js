a2ea91994a40a7a7ec46b2d0d77a85e75c7d29d7
Benuts159753 <blettnerbenoit@gmail.com>
2018-10-18 16:57:47 +0200

    wip welcome page

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// function numberGen (on page load){
+// var randomNumber = Math.floor(Math.random()*15 + 1);
+//    if (randomNumber == 1) {
+//     display link produit at div;
+//    } else if (randomNumber == 2) {
+//       display link produit at div
+//    } else if (randomNumber == 3) {
+//       display link produit at div
+//    } else if (randomNumber == 4) {
+//       display link produit at div
+//    } else if (randomNumber == 5) {
+//       display link produit at div
+//    } else if (randomNumber == 6) {
+//       display link produit at div
+//    } else if (randomNumber == 7) {
+//       display link produit at div
+//    } else if (randomNumber == 8) {
+//       display link produit at div
+//    } else if (randomNumber == 9) {
+//       display link produit at div;
+//    } else if (randomNumber == 10) {
+//       display link produit at div;
+//    } else if (randomNumber == 11) {
+//       display link produit at div;
+//    } else if (randomNumber == 12) {
+//       display link produit at div;
+//    } else if (randomNumber == 13) {
+//       display link produit at div;
+//    } else if (randomNumber == 14) {
+//       display link produit at div;
+//    } else {
+//      display link produit at div;
+// }
+// }
